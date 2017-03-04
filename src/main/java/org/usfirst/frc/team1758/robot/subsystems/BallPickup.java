9db79f3cc2a005deb04c46487d59b2d64f435d3a
Will Dixon <dixonwille@hotmail.com>
2017-03-03 22:14:23 -0500

    Cleaning a few files

--- src/main/java/org/usfirst/frc/team1758/robot/subsystems/BallPickup.java
+++ src/main/java/org/usfirst/frc/team1758/robot/subsystems/BallPickup.java
@@ -1,12 +1,10 @@
 package org.usfirst.frc.team1758.robot.subsystems;
 
 import org.usfirst.frc.team1758.robot.RobotMap;
-import org.usfirst.frc.team1758.robot.subsystems.DriveTrain.Motor;
 
 import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
